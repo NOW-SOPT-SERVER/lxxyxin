d49041c6708d16e6dbf02725a3294d55621818b7
gardening-y <yj4185@duksung.ac.kr>
2024-04-17 18:43:04 +0900

    chore: 패키지 정리

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+X
+(src/main/java/com/sopt/classes/Bank.java,3/c/3cbea1b6e557ce7b91b05666a39a6ed827b3cb47
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
---
+++ bank/src/main/java/com/sopt/classes/Bank.java
@@ -0,0 +1,36 @@
+package com.sopt.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private List<Customer> customerList;
+    private List<Account> accountList = new ArrayList<>();
+
+    public List<Account> getAccountList() {
+        return accountList;
+    }
+
+    public List<Customer> getCustomerList() {
+        return customerList;
+    }
+
+    //생성자
+    public Bank(){}
+
+    //계좌 존재 여부 확인
+    public boolean findByAccountNumber(String accountNumber){
+        for (Account account : this.accountList) {
+            if (!account.getAccountNumber().equals(accountNumber)) {
+                return false; //계좌가 존재하지 않으면 false 반환
+            }
+        }
+        return true;
+    }
+
+    public void addAccount(Account account){
+        accountList.add(account);
+    }
+
+
+}
