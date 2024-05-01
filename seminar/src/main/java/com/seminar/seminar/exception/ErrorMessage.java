86a69fb74dbc4cc0f3d4f6d9890a372be4cff999
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-05-02 01:30:10 +0900

    feat : ErrorMessage 수정

--- seminar/src/main/java/com/seminar/seminar/exception/ErrorMessage.java
+++ seminar/src/main/java/com/seminar/seminar/exception/ErrorMessage.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorMessage {
     MEMBER_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 사용자가 존재하지 않습니다."),
     BLOG_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 블로그가 존재하지 않습니다."),
+    BLOG_NOT_FOUND_BY_MEMBER_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "해당 사용자의 블로그가 존재하지 않습니다."),
     POST_NOT_FOUND_BY_BLOG_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(),"해당 블로그에 게시물이 존재하지 않습니다."),
     POST_NOT_FOUND_BY_ID_EXCEPTION(HttpStatus.NOT_FOUND.value(), "ID에 해당하는 게시글이 존재하지 않습니다.")
     ;
